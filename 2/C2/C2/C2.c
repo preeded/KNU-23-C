12881193cca73a60db6d4e8370d6c49ff0d60c9d
preeded <86511859+preeded@users.noreply.github.com>
2023-09-07 10:25:49 +0900

    Create C2.c

---
+++ 2/C2/C2/C2.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    int v, j=1;
+    scanf("%d", &v);
+    printf("%d층 피라미드\n", v);
+    for (int i = 0; i < v; i++) {
+        for (int l = 0; l < v - 1 - i; l++) {
+            printf(" ");
+        }
+        for (int k = 0; k < j; k++) {
+            printf("*");
+        }
+        printf("\n");
+        j += 2;
+    }
+}
